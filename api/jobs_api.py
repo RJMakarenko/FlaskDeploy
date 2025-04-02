393ebe85f0bfc964f9756826ce017d45acc927dd
RJMakarenko <propusk2@mail.ru>
2025-04-02 20:53:06 +0300

    timeweb deploy6

--- api/jobs_api.py
+++ api/jobs_api.py
@@ -9,6 +9,7 @@ jobs_bp = flask.Blueprint('jobs_api', __name__, template_folder='templates')
 
 @jobs_bp.route('/jobs')
 def get_jobs():
+    db_session.global_init('db/mars.db')
     session = db_session.create_session()
     jobs = session.query(Jobs).all()
     return flask.jsonify({'jobs': ([item.to_dict(
@@ -19,6 +20,7 @@ def get_jobs():
 
 @jobs_bp.route('/jobs/<int:job_id>', methods=['GET'])
 def get_one_job(job_id):
+    db_session.global_init('db/mars.db')
     session = db_session.create_session()
     job = session.query(Jobs).get(job_id)
     return flask.jsonify({'jobs': ([job.to_dict(
